e6ff155d608c72b9527d679174b577c3ef0f29f2
Rafael Vega <email.rafa@gmail.com>
2013-02-07 11:39:09 -0500

    Facebook login

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -2,10 +2,32 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
   attr_accessible :provider, :uid
+
+  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    unless user
+      user = User.create(name:auth.extra.raw_info.name,
+                         provider:auth.provider,
+                         uid:auth.uid,
+                         email:auth.info.email,
+                         password:Devise.friendly_token[0,20]
+                        )
+      # TODO: If user exists in our database and this is the first time they log in with facebook
+    end
+    user
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
 end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,7 +1,7 @@
 Preguntas::Application.routes.draw do
-  devise_for :users
-
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :users
+  
   resources :questions
   root :to => 'questions#index'
 
