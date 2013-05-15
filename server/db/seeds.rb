f31993576acab2e389427a87647baa75834704d4
Rafael Vega <email.rafa@gmail.com>
2013-05-15 11:45:55 -0500

    seed

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -17,17 +17,22 @@ class Pregunta < ActiveRecord::Base
   attr_accessible :id, :usuario_id, :extrana, :topico, :cuerpo, :score
 end
 
-Usuario.create :id=>1, 
-  :nombre=>"Admin", 
-  :email=>"admin@admin.com", 
-  :password=>"lalala121212", 
-  :password_confirmation=>"lalala121212", 
-  :rol=>'admin',
-  :confirmed_at => DateTime.new
 
-Pregunta.create :id=>1,
-  :topico=>"¿Cómo podemos mejorar la aplicación de La República Ciudadana?",
-  :cuerpo=>"Esta pregunta está claramente fuera de tópico pero la quisimos incluir para poder preguntarles a ustedes, los usuarios, que sugerencias o críticas tienen para nuestra aplicación. <br><br>Animense a participar y gracias por ayudarnos a mejorar!",
-  :usuario_id=>1,
-  :extrana=>1,
-  :score=>0
+if Usuario.find_by_id(1).nil?
+  Usuario.create :id=>1, 
+    :nombre=>"Admin", 
+    :email=>"admin@admin.com", 
+    :password=>"lalala121212", 
+    :password_confirmation=>"lalala121212", 
+    :rol=>'admin',
+    :confirmed_at => DateTime.new
+end
+
+if Pregunta.find_by_id(1).nil?
+  Pregunta.create :id=>1,
+    :topico=>"¿Cómo podemos mejorar la aplicación de La República Ciudadana?",
+    :cuerpo=>"Esta pregunta está claramente fuera de tópico pero la quisimos incluir para poder preguntarles a ustedes, los usuarios, que sugerencias o críticas tienen para nuestra aplicación. <br><br>Animense a participar y gracias por ayudarnos a mejorar!",
+    :usuario_id=>1,
+    :extrana=>1,
+    :score=>0
+end
